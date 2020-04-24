de45f2cc05383b3b47cf5b4aa0f2e3fd2d3ca3ad
Grant Simmons <gsimm706@gmail.com>
2020-04-23 21:19:22 -0400

    Add script modularity

--- scripts/asm_to_bit.py
+++ scripts/asm_to_bit.py
@@ -1,11 +1,22 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', default=None)
+parser.add_argument('-o', '--output', default=None)
+args = parser.parse_args()
+
 with open("../alias/ops.alias", 'r') as alias:
     op_alias = {} #OP hash table
     for op_line in alias:
         (bin_rep, curr_op) = op_line.split(' ')
         op_alias[curr_op.strip()] = int(bin_rep, 2) #Instruction Mnemonic = Key, Op Code = Value
-    with open("stim.asm", 'r') as asmfile:
-        with open("stim.bin", 'w+b') as binfile:
-            for line in asmfile:
-                op = line.strip() #Get stimulus instruction (ASM)
-                if op in op_alias:
-                    binfile.write(op_alias.get(op, 'NOP').to_bytes(1, 'little')) #Write ASCII binary
+    if args.file:
+        with open(args.file, 'r') as asmfile:
+            with open("{}.bin".format(args.file.split('.')[0]), 'w+b') as binfile:
+                for line in asmfile:
+                    op = line.strip() #Get stimulus instruction (ASM)
+                    if op in op_alias:
+                        binfile.write(op_alias.get(op, 'NOP').to_bytes(1, 'little')) #Write ASCII binary
+    else:
+        sys.exit("Please specify a file to convert using -f")
