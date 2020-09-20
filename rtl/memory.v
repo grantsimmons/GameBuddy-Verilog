96b73564ad6e57dca679a9b0ff0ffd4350cab0c7
Grant Simmons <gsimm706@gmail.com>
2020-09-20 14:55:41 -0500

    Add read/write protections to memory regions

--- rtl/memory.v
+++ rtl/memory.v
@@ -23,14 +23,55 @@ module memory #(parameter ADDR_WIDTH = 16, DATA_WIDTH = 8) (
     always @(rd_en or wr_en or data_in or addr_bus) begin //Warning: Write capabilites synthesize as register list (Distributed RAM)
         //TODO: Invert enable polarities from CPU internal
         if(rd_en) begin
+
+            //TODO: Move ROM to its own module
+            if(addr_bus >= 16'h0000 && addr_bus < 16'h8000) begin //ROM
+                if(addr_bus < 16'h0100) begin //BIOS
+                    data_out <= mem[addr_bus]; //TODO: BIOS Switch
+                end //BIOS
+                else begin //Cart ROM
+                    data_out <= mem[addr_bus];
+                end //Cart ROM
+            end //ROM
+
+            if(addr_bus >= 16'h8000 && addr_bus < 16'hA000) begin //VRAM
+                //TODO: Move VRAM to Graphics module
+                data_out <= mem[addr_bus];
+            end //VRAM
+
+            if(addr_bus >= 16'hA000 && addr_bus < 16'hC000) begin //Switchable RAM
+                //TODO: Move Switchable RAM to RAM module
+                data_out <= mem[addr_bus];
+            end //Switchable RAM
+
+            if(addr_bus >= 16'hC000 && addr_bus < 16'hE000) begin //Internal RAM
+                data_out <= mem[addr_bus];
+            end //Internal RAM
+
+            if(addr_bus >= 16'hE000 && addr_bus < 16'hFE00) begin //Echo RAM
+                data_out <= mem[addr_bus & 16'hDFFF];
+            end //Echo RAM
+
             if(addr_bus >= 16'hFF00 && addr_bus < 16'hFF40)
                 data_out <= 8'b0;
             else
                 data_out <= mem[addr_bus];
         end
+
         if(wr_en) begin
-            if(addr_bus >= 16'h8000)
+            //ROM
+            
+            if(addr_bus >= 16'h8000 && addr_bus < 16'hA000) begin //VRAM
                 mem[addr_bus] <= data_in;
+            end //VRAM
+
+            if(addr_bus >= 16'hA000 && addr_bus < 16'hC000) begin //Cart RAM (If available)
+                mem[addr_bus] <= data_in;
+            end //Cart RAM
+
+            if(addr_bus >= 16'hC000 && addr_bus < 16'hFE00) begin
+                mem[addr_bus & 16'hDFFF] <= data_in;
+            end
         end
         //data_out <= 8'b0;
     end
